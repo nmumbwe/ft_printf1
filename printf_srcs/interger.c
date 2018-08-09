05cff83576ac218b5204feb485eb79be9402df05
Nawa MUMBWE <nmumbwe@e4r12p5.wethinkcode.co.za>
2018-08-09 09:28:36 +0200

    yet again'

--- printf_srcs/interger.c
+++ printf_srcs/interger.c
@@ -6,7 +6,7 @@
 /*   By: nmumbwe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/19 14:00:45 by nmumbwe           #+#    #+#             */
-/*   Updated: 2018/08/09 09:12:27 by nmumbwe          ###   ########.fr       */
+/*   Updated: 2018/08/07 15:12:59 by nmumbwe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,31 @@
 
 int		right_just_inte(char *temp, int i, t_flag *flag)
 {
-	char a;
+	char	a;
+	int		b;
 
-	if (flag->period == 1)
-		flag->zero = 0;
 	a = flag->zero == 1 ? '0' : ' ';
-	if (flag->plus == 1)
+	b = flag->width - flag->max_width;
+	if (flag->space == 1)
+	{
 		i--;
+		ft_putnchar(' ', flag);
+	}
+	while (b-- > 0)
+		ft_putnchar(' ', flag);
+	if (flag->plus == 1 && *temp != '-' && flag->zero == 1)
+		ft_putnchar('+', flag);
 	while (i-- > 0)
 		ft_putnchar(a, flag);
-	if (flag->plus == 1)
+	if (flag->plus == 1 && *temp != '-' && flag->zero == 0)
 		ft_putnchar('+', flag);
 	while (*temp)
 		ft_putnchar(*(temp)++, flag);
 	return (1);
 }
 
-int		inte_p(char *temp, t_flag *flag)
+int		inte_p(char *temp, t_flag *flag, int i)
 {
-	int	i;
-
-	i = flag->width - ft_strlen(temp);
 	if (flag->minus == 1)
 	{
 		if (flag->plus == 1 && *temp != '-')
@@ -58,11 +62,18 @@ int		interger(t_flag *flag, va_list lst, t_length *length)
 	char			*temp;
 	int				i;
 	int				j;
+	int				len;
 
 	j = 0;
 	i = get_num(flag, length, lst);
 	temp = ft_itoa_base(i, 10);
-	inte_p(temp, flag);
+	len = flag->max_width - ft_strlen(temp);
+	if (flag->period == 1 && flag->space == 1)
+	{
+		flag->zero = 1;
+		len = flag->width;
+	}
+	inte_p(temp, flag, len);
 	free(temp);
 	return (1);
 }
